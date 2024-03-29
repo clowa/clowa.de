49ac4e85f8798cc402468e88da006d691dfc58f5
Cedric Ahlers <55838528+clowa@users.noreply.github.com>
2024-03-29 14:02:40 +0100

    update backend function to fetch quote from custom quote API

--- api/GetQuote/index.ts
+++ api/GetQuote/index.ts
@@ -5,10 +5,9 @@ interface IQoute {
   content: string;
   author: string;
   authorSlug: string;
-  tags: string[];
   length: number;
-  dateAdded: Date;
-  dateModified: Date;
+  tags: string[];
+  creationDate: Date;
 }
 
 const httpTrigger: AzureFunction = async function (
@@ -18,18 +17,38 @@ const httpTrigger: AzureFunction = async function (
   context.log("HTTP trigger function processed a request.");
   console.log("Received request: ", req.headers.host);
 
+  let api_key = process.env.API_KEY;
+  if (!api_key) {
+    console.error("Environment variable API_KEY is not set.");
+    context.res = {
+      status: 500,
+      body: "Internal Server Error",
+    };
+    return;
+  }
+
+  let headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "Ocp-Apim-Subscription-Key": process.env.API_KEY,
+  };
+
   try {
-    const response = (await fetch("https://api.quotable.io/random")).json();
+    const response = (
+      await fetch("https://api.clowa.dev/api/quote", {
+        method: "GET",
+        headers: headers,
+      })
+    ).json();
     const quote = (await response) as IQoute;
 
-    const responseMessage = quote.content;
     context.res = {
       Headers: {
         "Content-Type": "plain/text",
         "Allow-Control-Allow-Origin": "*",
       },
       status: 200 /* Defaults to 200 */,
-      body: responseMessage,
+      body: JSON.stringify(quote),
     };
   } catch (err) {
     console.error("Failed to call API: ", err);
