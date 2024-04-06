835b804854383a1a9dbb58c5cefb9c12720cdee3
Cedric Ahlers <55838528+clowa@users.noreply.github.com>
2024-04-06 22:41:21 +0200

    handle timeout error of quote api fetch

--- swa/src/scripts/loadQuote.js
+++ swa/src/scripts/loadQuote.js
@@ -10,19 +10,25 @@ let quote = {
 // Get current website name
 const canonicalURL = location.protocol + "//" + location.host
 console.log("Detected website name " + canonicalURL);
-const controller = new AbortController();
-const id = setTimeout(() => controller.abort(), timeout);
-const response = await fetch(canonicalURL + "/api/getQuote", {signal: controller.signal});
-clearTimeout(id);
 
-// Check if the API is available and default to a static quote if not.
-if (response.ok) {
-  const data = await response.json();
-  quote = {
-    content: data.content,
-    author: data.author
+try {
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+  const response = await fetch(canonicalURL + "/api/getQuote", {signal: controller.signal});
+  clearTimeout(id);
+
+  // Check if the API is available and default to a static quote if not.
+  if (response.ok) {
+    const data = await response.json();
+    quote = {
+      content: data.content,
+      author: data.author
+    }
   }
+} catch (error) {
+  console.error("Failed to fetch quote from API: " + error);
 }
+
 document.getElementById("quote-text").innerText = quote.content;
 document.getElementById("quote-author").innerText = quote.author;
 document.getElementById("quote-loader").remove();
