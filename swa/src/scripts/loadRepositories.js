bc43feaf45b3380c3d9234316bc43279a319c78f
Cedric Ahlers <55838528+clowa@users.noreply.github.com>
2024-08-15 18:07:50 +0000

    hardcode repos as long as repos are only fetched at build time

--- swa/src/scripts/loadRepositories.js
+++ swa/src/scripts/loadRepositories.js
@@ -1,9 +1,24 @@
-async function loadRepositories(count = 6) {
-  const response = await fetch(`https://api.github.com/users/clowa/repos?sort=pushed&direction=desc&per_page=${count}`);
-  if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
+// async function loadRepositories(count = 6) {
+//   const response = await fetch(`https://api.github.com/users/clowa/repos?sort=pushed&direction=desc&per_page=${count}`);
+//   if (!response.ok) {
+//       throw new Error('Network response was not ok ' + response.statusText);
+//   }
+//   const repos = await response.json();
+//   return repos;
+// }
+
+async function loadRepositories(repositories = array(string)) {
+  const repos = [];
+
+  for (const r of repositories) {
+    const response = await fetch(`https://api.github.com/repos/clowa/${r}`);
+    if (!response.ok) {
+      throw new Error(`Network response was not ok ${response.statusText}`);
+    }
+    const repo = await response.json();
+    repos.push(repo);
   }
-  const repos = await response.json();
+
   return repos;
 }
 
